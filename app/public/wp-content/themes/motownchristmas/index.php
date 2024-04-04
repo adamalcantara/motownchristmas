42c5b3badf07034e82bf42c13e1ab826f38db8c7
adamalcantara <alcantaradevelopment@gmail.com>
2024-04-03 22:48:12 -0400

    Sections added to index.php

--- app/public/wp-content/themes/motownchristmas/index.php
+++ app/public/wp-content/themes/motownchristmas/index.php
@@ -4,18 +4,36 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
-    <div id="header">
+<body <?php body_class(); ?>>
+    <div id="page" class="site">
+        <div id="header">
 
-    </div>
-    <div id="video">
+        <!-- Menu goes here -->
+            <nav class="main-menu">
 
-    </div>
-    <div id="description">
+            </nav>
+        </div>
 
-    </div>
-    <div id="footer">
-        
+        <!-- Site content box -->
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+
+                <!-- Homepage sections -->
+                    <section class="video">
+
+                    </section>
+                    <section class="intro">
+
+                    </section>
+                </main>
+            </div>
+        </div>
+
+        <!-- Footer goes here -->
+        <div id="footer">
+            
+        </div>
     </div>
 </body>
 </html>
