ec4b259ee4623c3963aa9116befae7727291b551
adamalcantara <alcantaradevelopment@gmail.com>
2024-04-04 23:57:56 -0400

    Commenting and organizing

--- app/public/wp-content/themes/motownchristmas/header.php
+++ app/public/wp-content/themes/motownchristmas/header.php
@@ -4,8 +4,11 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
+<!-- Body section -->
 <body <?php body_class(); ?>>
     <div id="page" class="site">
+        <!-- Header section -->
         <header id="header">
         Logo
         <!-- Menu goes here -->
