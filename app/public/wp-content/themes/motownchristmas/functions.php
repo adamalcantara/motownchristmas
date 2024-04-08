5a3d892a3263dbf27560e50ec0a5c354103fa81c
adamalcantara <alcantaradevelopment@gmail.com>
2024-04-08 00:02:45 -0400

    Enqueued Roboto font from Google fonts

--- app/public/wp-content/themes/motownchristmas/functions.php
+++ app/public/wp-content/themes/motownchristmas/functions.php
@@ -1,7 +1,10 @@
 <?php 
 
 function motownchristmas_load_scripts() {
-    wp_enqueue_style( 'motownchristmas-style', get_stylesheet_uri(), array(), '1.0', 'all' );
+    wp_enqueue_style( 'motownchristmas-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ), 'all' );
+
+    // enqueuing fonts
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700;900&display=swap', array(), null );
 }
 
 add_action( 'wp_enqueue_scripts', 'motownchristmas_load_scripts' );
