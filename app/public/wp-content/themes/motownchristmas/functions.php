500155e793d4c3172dabe650e386e18ef938ff8d
adamalcantara <alcantaradevelopment@gmail.com>
2024-04-07 23:49:05 -0400

    Added action in function

--- app/public/wp-content/themes/motownchristmas/functions.php
+++ app/public/wp-content/themes/motownchristmas/functions.php
@@ -4,4 +4,6 @@ function motownchristmas_load_scripts() {
     wp_enqueue_style( 'motownchristmas-style', get_stylesheet_uri(), array(), '1.0', 'all' );
 }
 
+add_action( 'wp_enqueue_scripts', 'motownchristmas_load_scripts' );
+
 ?>
